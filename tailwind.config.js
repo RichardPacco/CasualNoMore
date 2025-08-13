84caa18bf6a36b95928968d1349065750cae5959
Richard Pacco <richard.pacco@live.com>
2025-08-13 16:15:52 -0300

    styles para native wind configurados, jsx adicionado no tsconfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./app/**/*.{ja,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {},
   },

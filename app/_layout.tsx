eb480ce1905c5c7165295a997bc4f91451fe4176
Richard Pacco <richard.pacco@live.com>
2025-08-14 12:55:53 -0300

    adiciona safearea no cabeçalho que respeita o thema de cores do aparelho

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,26 @@
-import { Stack } from "expo-router";
 import './globals.css';
+import { Stack } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar, useColorScheme } from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  const colorScheme = useColorScheme();
+  const isDark = colorScheme === "dark";
+
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: isDark ? "#111" : "#fff", // muda conforme tema
+      }}
+      edges={["top", "left", "right"]}
+    >
+      <StatusBar
+        barStyle={isDark ? "light-content" : "dark-content"} // texto conforme tema
+        backgroundColor={isDark ? "#111" : "#fff"}           // cor de fundo conforme tema
+      />
+      <Stack screenOptions={{ headerShown: false }} />
+    </SafeAreaView>
+  );
 }
+

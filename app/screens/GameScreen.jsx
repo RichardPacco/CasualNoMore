09d34a8bc2900d0ae9f01d8bed821def32eeded6
Richard Pacco <richard.pacco@live.com>
2025-08-14 14:23:15 -0300

    melhora botoes do tab selector

--- app/screens/GameScreen.jsx
+++ app/screens/GameScreen.jsx
@@ -1,7 +1,7 @@
-import { useState, useEffect } from "react";
-import { View, FlatList } from "react-native";
+import { useEffect, useState } from "react";
+import { FlatList, View } from "react-native";
+import { getOwnedGames } from "../../src/api/steam";
 import GameCard from "../../src/components/GameCard";
-import { getOwnedGames } from "../../src/api/steam"; // make sure this calls Steam API correctly
 
 export default function GameScreen({ navigation }) {
     const [games, setGames] = useState([]);
@@ -22,7 +22,9 @@ export default function GameScreen({ navigation }) {
                 renderItem={({ item }) => (
                     <GameCard game={item} navigation={navigation} />
                 )}
+                contentContainerStyle={{ paddingBottom: 80 }}
             />
         </View>
     );
+
 }
